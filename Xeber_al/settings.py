c7f3bb9f8a883afdb9da8d56fdbfcc1f9803e01d
sah-cyber <vip.shahin@list.ru>
2025-10-15 11:31:42 +0400

    Update ALLOWED_HOSTS to include Render domain

--- Xeber_al/settings.py
+++ Xeber_al/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'false').lower() =="true"
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['xeberal.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
