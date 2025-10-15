5dbb4add1049a42698662280004ce538d70b66bc
sah-cyber <vip.shahin@list.ru>
2025-10-15 11:46:24 +0400

    Fix static files configuration: add STATIC_ROOT and update STATIC_URL

--- Xeber_al/settings.py
+++ Xeber_al/settings.py
@@ -123,12 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # mütləq slash ilə başlasın
 STATICFILES_DIRS = [
     BASE_DIR / "static",   # Layihə kökündə static/ qovluğu
 ]
-MEDIA_ROOT = os.path.join('media') # 'data' is my media folder
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic əməli bu qovluğa yığacaq
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
