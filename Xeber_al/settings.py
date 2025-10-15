f8faa7763ed97fffdf7ebc11a194c57a154ca753
sah-cyber <vip.shahin@list.ru>
2025-10-15 12:12:49 +0400

    Fix static files configuration: add STATIC_ROOT and update STATIC_URLrr

--- Xeber_al/settings.py
+++ Xeber_al/settings.py
@@ -51,8 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'Xeber_al.urls'
 
 TEMPLATES = [
@@ -122,17 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'  # mütləq slash ilə başlasın
-STATICFILES_DIRS = [
-    BASE_DIR / "static",   # Layihə kökündə static/ qovluğu
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic əməli bu qovluğa yığacaq
-
+# STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic əməli bu qovluğa yığacaq
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+#
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
